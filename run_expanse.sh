76a7a4ba83b98e3fb0b94e7ab661b4df31d6b88b
Siddharth Sonti <83441624+Sonti974948@users.noreply.github.com>
2024-11-04 10:13:18 -0800

    Update run_expanse.sh

--- run_expanse.sh
+++ run_expanse.sh
@@ -24,9 +24,8 @@ module load amber/20
 export PATH=$PATH:$HOME/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 source $AMBERHOME/amber.sh
+tleap -f tleap.in
 #pmemd.cuda -O -i min.in -o min.out -p ALA.prmtop -c ALA.rst -r min.rst -x min.nc -AllowSmallBox
-#tleap -f tleap.in
 #pmemd.cuda -O -i heat.in -o heat.out -p ALA.prmtop -c min.rst -r heat.rst -x heat.nc -ref min.rst -AllowSmallBox
-pmemd.cuda -O -i prod.in -o prod.out -p ALA.prmtop -c heat.rst -r prod.rst -x prod.nc -ref heat.rst -AllowSmallBox
-#pmemd.cuda -O -i eq2.in -o eq2.out -p ALA.prmtop -c eq1.rst -r eq2.rst -x eq2.nc -ref eq1.rst
-#pmemd.cuda -O -i md_gamd.in -o md_gamd.out -p ALA.prmtop -c ALA.rst -r md_gamd.rst -x md_gamd.nc
+#pmemd.cuda -O -i prod.in -o prod.out -p ALA.prmtop -c heat.rst -r prod.rst -x prod.nc -ref heat.rst -AllowSmallBox
+
